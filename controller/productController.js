e7d93792f317f321795360b3e72179aafc60e323
ntandomabuto <ntandomabuto@gmail.com>
2024-08-20 13:47:23 +0200

    insert branch

--- controller/productController.js
+++ controller/productController.js
@@ -9,6 +9,12 @@ const getOne = async (req,res)=>{
     res.json(await getOneDb(req.params.id))
 }
 
+const insertOne = async (req,res)=>{
+    let {prod_name,quantity,amount,category,prod_url} = req.body 
+    await insertOneDb(prod_name,quantity,amount,category,prod_url)
+    res.send('Inserted a product')
+}
+
 
 
 export {getPro,getOne,insertOne,deleteOne,updateOne}
--- controller/userController.js
+++ controller/userController.js
@@ -9,6 +9,16 @@ const fetchUser = async(req,res)=>{
     res.json(await fetchUserDb(req.params.id))
 }
 
+const insertUser = async(req,res)=>{
+    let {firstname,lastname,user_age,gender,user_role,email_add,user_pass,user_profile} = req.body
+    bcrypt.hash(user_pass,10,async (error,result)=>{
+        if(error) throw error
+        console.log(result);
+        await insertUserDb(firstname,lastname,user_age,gender,user_role,email_add,result,user_profile)
+        res.send('Inserted data sucessfully')
+    })
+}
+
 
 
 export {fetchUsers,fetchUser,insertUser,deleteUser,updateUser}
