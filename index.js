c183dd3dd78455891afc0078905ee5f842d227d6
ntandomabuto <ntandomabuto@gmail.com>
2024-08-24 12:27:35 +0200

    cors

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import productRoute from './routes/productRoute.js'
 let port = process.env.PORT || 2024
 
 const app = express()
-app.use(cors({
-    origin: "*",
-    credentials:true
-}))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+})
+app.use(cors())
 app.use(express.json())
 app.use('/user',useRoute)
 app.use('/product',productRoute)
